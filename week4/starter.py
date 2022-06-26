ef2fff337c8be450541e5e7b9e1a230a9a0913cd
Tim Becker <tim_becker@online.de>
2022-06-26 11:54:13 +0200

    questions 1 to 3 of week4 homework

---
+++ week4/starter.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pickle
+import pandas as pd
+
+
+with open('model.bin', 'rb') as f_in:
+    dv, lr = pickle.load(f_in)
+
+
+def read_data(filename):
+    df = pd.read_parquet(filename)
+    
+    df['duration'] = df.dropOff_datetime - df.pickup_datetime
+    df['duration'] = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)].copy()
+
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+    return df
+
+
+def write_results(df, output_file):
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = lr.predict(X_val)
+
+    df_result = pd.DataFrame()
+    df_result['ride_id'] = df['ride_id']
+    df_result['predictions'] = y_pred
+
+    df_result.to_parquet(
+        output_file,
+        engine='pyarrow',
+        compression=None,
+        index=False
+    )
+
+
+year = 2021
+month = 2
+input_file = f'https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_{year}-{month:02d}.parquet'
+output_file = f'result_fhv_tripdata_{year}-{month:02d}.parquet'
+categorical = ['PUlocationID', 'DOlocationID']
+
+
+if __name__ == '__main__':
+    data = read_data(input_file)
+    write_results(data, output_file)
